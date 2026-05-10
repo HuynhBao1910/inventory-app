baddd5b61d7e36ce46373b8e82f22926c9658e43
ChiBao <hpcbao12c1ntt@gmail.com>
2026-05-10 16:17:38 +0700

    fix port render

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,24 @@
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
 
 const dashboardRoute = require("./routes/dashboard");
 
-//middlware
+// middleware
 app.use(cors());
 app.use(express.json());
 
+// routes
+app.use("/api/auth", require("./routes/auth"));
 app.use("/api/sanpham", require("./routes/sanpham"));
-
 app.use("/api/donhang", require("./routes/donhang"));
-
 app.use("/api/dashboard", dashboardRoute);
 
-//routes
-app.use("/api/auth", require("./routes/auth"));
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-    console.log("SV đang chạy cổng 5000");
+app.listen(PORT, () => {
+    console.log(`Server đang chạy cổng ${PORT}`);
 });
