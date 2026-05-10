213b4e9033ea18899636f2c553047b255db9fe50
ChiBao <hpcbao12c1ntt@gmail.com>
2026-05-10 22:18:20 +0700

    re ver 16

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,8 @@ const dashboardRoute = require("./routes/dashboard");
 // middleware
 // const cors = require('cors');
 app.use(cors({
-    // origin: 'https://inventory-app-huynhbao1910s-projects.vercel.app',
-    origin: allowedOrigin,
+    origin: 'https://inventory-app-huynhbao1910s-projects.vercel.app',
+    // origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
